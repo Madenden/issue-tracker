025421d2aea191a31b8dd877c8ce53b757f977bd
Deniss Matvejevs <mden7785@gmail.com>
2018-09-07 16:38:12 +0000

    Include payment method for Feature tickets

--- tickets/models.py
+++ tickets/models.py
@@ -7,7 +7,7 @@ from model_utils import Choices
 
 
 # Create your models here.
-class Ticket(models.Model):
+class BugTicket(models.Model):
     author = models.ForeignKey(User, default=None)
     title = models.CharField(max_length=254, default="")
     description = models.TextField()
@@ -17,6 +17,30 @@ class Ticket(models.Model):
     status = StatusField()
     status_changed = MonitorField(monitor='status')
     
+    
+    ISSUE_STATUS = Choices('bug', 'feature')
+    issue_status = StatusField(choices_name='ISSUE_STATUS')
+    
+    
+    def __str__(self):
+        return self.title
+        
+    def snippet(self):
+        return self.description[:150] + "..."
+        
+class FeatureTicket(models.Model):
+    author = models.ForeignKey(User, default=None)
+    title = models.CharField(max_length=254, default="")
+    description = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    views = models.IntegerField(default=0)
+    STATUS  = Choices('to do', 'doing', 'done')
+    status = StatusField()
+    status_changed = MonitorField(monitor='status')
+    
+    ISSUE_STATUS = Choices('feature', 'bug')
+    issue_status = StatusField(choices_name='ISSUE_STATUS')
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
     
     def __str__(self):
         return self.title
--- tickets/views.py
+++ tickets/views.py
@@ -1,27 +1,61 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
-from .models import Ticket
-from .forms import CreateTicket
+from .models import BugTicket
+from .forms import BugTicketForm, FeatureTicketForm
+from .forms import MakePaymentForm
+from django.contrib import messages
+from django.conf import settings
+import stripe
+
+stripe.api_key = settings.STRIPE_SECRET
 
 # Create your views here.
 def tickets(request):
-    tickets = Ticket.objects.all().order_by("created_date")
+    tickets = BugTicket.objects.all().order_by("created_date")
     return render(request, "tickets.html", {'tickets': tickets})
 
 @login_required(login_url="/login/")    
 def create_ticket(request):
     if request.method == "POST":
-        form = CreateTicket(request.POST)
+        form = BugTicketForm(request.POST)
         if form.is_valid():
             instance = form.save(commit=False)
             instance.author = request.user
             instance.save()
             return redirect(reverse('tickets'))
-    else:
-        form = CreateTicket()
+        form = BugTicketForm()
     return render(request, "create-ticket.html", {'form': form})
     
 @login_required(login_url="/login/")
 def create_feature(request):
-    form = CreateTicket()
-    return render(request, "create-feature.html", {'form': form})
+    if request.method=="POST":
+        feature_ticket_form = FeatureTicketForm(request.POST)
+        payment_form = MakePaymentForm(request.POST)
+        
+        if feature_ticket_form.is_valid() and payment_form.is_valid():
+            feature_ticket = feature_ticket_form.save(commit=False)
+            feature_ticket.author = request.user
+            feature_ticket.save()
+            
+            try:
+                transaction = stripe.Charge.create(
+                    amount = int(feature_ticket.amount*100),
+                    currency = "EUR",
+                    description = "Feature Ticket",
+                    card = payment_form.cleaned_data['stripe_id'],
+                )
+            except stripe.error.CardError:
+                messages.error(request, "Your card was declined!")
+                
+            if transaction.paid:
+                messages.error(request, "You have successfully paid")
+                return redirect(reverse('tickets'))
+            else:
+                messages.error(request, "Unable to take payment")
+        else:
+            print(payment_form.errors)
+            messages.error(request, "We were unable to take a payment with that card!")
+    else:
+        feature_ticket_form = FeatureTicketForm()
+        payment_form = MakePaymentForm()
+    return render(request, "create-feature.html", {'form': feature_ticket_form, 'payment_form': payment_form,  'publishable': settings.STRIPE_PUBLISHABLE})
